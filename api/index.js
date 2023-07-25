ae5c5de855fd480f0ee4635f399328fbf9ce44db
rory <rorysaxton@gmail.com>
2023-07-25 15:35:33 -0700

    response test

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-import { get } from '@vercel/edge-config'
+//import { get } from '@vercel/edge-config'
 const express = require('express')
 const app = express()
 app.use(express.static(__dirname + '/public'));
